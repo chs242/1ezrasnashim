b6d9e9f387ec28bef632e4db50938cc98be0da9d
Edward Stern <edwardstern242@gmail.com>
2020-03-02 12:57:53 +0000

    completed default layout. making the mobile nav and splitting it all into components

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -5,10 +5,10 @@
     <header class="bg-gray-200 flex lg:h-20">
       <div class="hidden lg:w-2/4 lg:inline-flex justify-center items-center">
         <a href="/" class="px-3 font-roboto text-sm font-semibold hover:text-purple-800 transition duration-100 ease-linear">Home</a>
-        <a href="/volunteer" class="px-3 font-roboto text-sm font-semibold hover:text-purple-800 transition duration-100 ease-linear">Volunteer</a>
-        <a href="/contact" class="px-3 font-roboto text-sm font-semibold hover:text-purple-800 transition duration-100 ease-linear">Contact</a>
-        <a href="/contact" class="px-3 font-roboto text-sm font-semibold hover:text-purple-800 transition duration-100 ease-linear">News & Press</a>
-        <a href="/about" class="px-3 font-roboto text-sm font-semibold hover:text-purple-800 transition duration-100 ease-linear">About</a>
+        <a href="/volunteer" class="px-3 font-roboto text-sm font-semibold hover:text-pink-900 transition duration-100 ease-linear">Volunteer</a>
+        <a href="/contact" class="px-3 font-roboto text-sm font-semibold hover:text-pink-900 transition duration-100 ease-linear">Contact</a>
+        <a href="/contact" class="px-3 font-roboto text-sm font-semibold hover:text-pink-900 transition duration-100 ease-linear">News & Press</a>
+        <a href="/about" class="px-3 font-roboto text-sm font-semibold hover:text-pink-900 transition duration-100 ease-linear">About</a>
         <!-- <a href="/news" class="">News & Press</a> -->
       </div>
 
@@ -23,7 +23,7 @@
       </div>
 
       <div class="hidden lg:w-2/4 lg:inline-flex justify-center items-center">
-        <a href="/donate" class="font-roboto text-sm font-semibold text-white hover:text-pink-600 bg-pink-600 hover:bg-white border-2 border-pink-600 rounded-full px-12 py-2 leading-normal shadow-2xl">Donate</a>
+        <a href="/donate" class="font-roboto text-sm font-semibold text-white hover:text-pink-600 bg-pink-600 hover:bg-white border-2 border-pink-600 rounded-full px-12 py-2 leading-normal shadow-md">Donate</a>
       </div>
       
       
@@ -35,44 +35,54 @@
       </div>
     </header>
 
-    <main class="main">
+    <main class="main min-h-screen">
       <slot/>
     </main>
 
-    <footer class="footer bg-gray-200 mt-40 flex flex-col">
-      <div id="subscribe" class="shadow-2xl">
-        <div class="flex justify-center w-full px-6 text-center">
-          <h1 class="font-source text-2xl text-purple-800 font-semibold">Keep up with the latest happenings at Ezras Nashim</h1>
+    <footer class="footer bg-gray-200 pb-4 md:pb-8 flex flex-col md:text-xs">
+      <div id="subscribe" class="shadow-md py-4 md:flex">
+        <div class="flex justify-center w-full px-6 text-center md:w-1/2 lg:pl-40">
+          <h1 class="font-source text-2xl lg:text-4xl lg:text-left text-pink-900 font-semibold">Keep up with the latest happenings at Ezras Nashim!</h1>
         </div>
-        <div class="flex justify-center w-full mt-4">
-          <input type="text" placeholder="Your Email..." class="font-roboto text-base text-center font-light border-2 rounded-full w-4/5 py-1 leading-normal shadow-2xl">
+        <div class="flex justify-center w-full mt-4 md:mt-0 md:inline-flex md:w-1/2 md:relative md:pr-8 md:items-center lg:px-32 lg:pr-48">
+          <input type="email" placeholder="Your Email..." class="font-roboto text-base text-center md:text-left font-light border-2 rounded-full w-4/5 min-w-40 md:w-full md:px-2 py-2 lg:py-3 leading-normal shadow-2xl">
+          <button href="#" class="hidden md:inline md:absolute md:right-0 md:w-1/3 lg:w-32 md:mr-4 lg:mr-24 xl:mr-40 font-roboto text-base font-semibold text-white hover:text-pink-600 bg-pink-600 hover:bg-white border-2 rounded-full w-4/5 py-2 lg:py-3 leading-normal shadow-2xl">Subscribe</button>
         </div>
-        <div class="flex justify-center w-full mt-4">
+        <div class="flex justify-center w-full mt-2 md:hidden">
           <button href="#" class="font-roboto text-base font-semibold text-white hover:text-pink-600 bg-pink-600 hover:bg-white border-2 rounded-full w-4/5 py-2 leading-normal shadow-2xl">Subscribe</button>
         </div>
       </div>
       <div id="footer-end" class="mt-4">
-        <div class="flex flex-col text-center">
+        <div class="md:hidden flex flex-col text-center">
           <div class="flex justify-center">
             <img src="../assets/images/logo-footer.png" alt="Logo" class="w-16">
           </div>
           <p>Email: leah@ezrasnashim.org</p>
           <p>Address: 1716 56th St. Brokklyn, NY 11204</p>
         </div>
-        <div class="w-full flex mt-4">
-          <div class="w-1/2 flex flex-col text-center">
-            <p class="w-full font-roboto text-base font-medium">Service</p>
-            <a href="/about" class="w-full font-source text-md font-medium">Our Team</a>
-            <a href="/volunteer" class="w-full font-source text-md font-medium">Our Services</a>
-            <a href="/endorsements" class="w-full font-source text-md font-medium">Endorsments</a>
-            <a href="/endorsements" class="w-full font-source text-md font-medium">About Us</a>
+        <div class="w-full flex mt-4 md:mt-24">
+          
+          <div class="w-1/2 flex flex-col text-center lg:text-left hover:text-purple-800 xl:pl-32">
+            <p class="w-full font-roboto text-sm font-medium md:mb-6 lg:w-1/2 lg:pl-40 xl:pl-64">Service</p>
+            <a href="/about" class="w-full font-source text-gray-700 text-xs font-medium lg:w-2/3 lg:pl-40 xl:pl-64">Our Team</a>
+            <a href="/volunteer" class="w-full font-source text-gray-700 text-xs font-medium lg:w-2/3 lg:pl-40 xl:pl-64">Our Services</a>
+            <a href="/endorsements" class="w-full font-source text-gray-700 text-xs font-medium lg:w-2/3 lg:pl-40 xl:pl-64">Endorsments</a>
+            <a href="/endorsements" class="w-full font-source text-gray-700 text-xs font-medium lg:w-2/3 lg:pl-40 xl:pl-64">About Us</a>
+          
           </div>
-          <div class="w-1/2 flex flex-col text-center">
-            <p class="w-full font-roboto text-base font-medium">Popular</p>
-            <a href="/about" class="w-full font-source text-md font-medium">News & Press</a>
-            <a href="/volunteer" class="w-full font-source text-md font-medium">Volunterr</a>
-            <a href="/endorsements" class="w-full font-source text-md font-medium">Donate</a>
-            <a href="/endorsements" class="w-full font-source text-md font-medium">Contact Us</a>
+          <div class="hidden md:inline text-center">
+          <div class="flex justify-center">
+            <img src="../assets/images/logo-footer.png" alt="Logo" class="w-16">
+          </div>
+          <p>Email: leah@ezrasnashim.org</p>
+          <p>Address: 1716 56th St. Brokklyn, NY 11204</p>
+        </div>
+          <div class="w-1/2 flex flex-col text-center lg:text-left hover:text-purple-800 xl:pr-32">
+            <p class="w-full font-roboto text-sm font-medium md:mb-6 lg:w-1/2 lg:pl-40 xl:pl-56">Popular</p>
+            <a href="/about" class="w-full font-source text-gray-700 text-xs font-medium lg:w-2/3 lg:pl-40 xl:pl-56">News & Press</a>
+            <a href="/volunteer" class="w-full font-source text-gray-700 text-xs font-medium lg:w-2/3 lg:pl-40 xl:pl-56">Volunterr</a>
+            <a href="/endorsements" class="w-full font-source text-gray-700 text-xs font-medium lg:w-2/3 lg:pl-40 xl:pl-56">Donate</a>
+            <a href="/endorsements" class="w-full font-source text-gray-700 text-xs font-medium lg:w-2/3 lg:pl-40 xl:pl-56">Contact Us</a>
           </div>
         </div>
       </div>
@@ -206,7 +216,4 @@ export default {
   margin: -1px;
 }
 
-.footer{
-  border: 2px solid red;
-}
 </style>
