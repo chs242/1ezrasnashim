98384a92e565d6ccbfe45cc24f3a110383608f8d
Edward Stern <edwardstern242@gmail.com>
2020-03-04 11:06:20 +0000

    touched up subscribe button

--- src/components/IndexCard.vue
+++ src/components/IndexCard.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="w-full md:w-64 mb-4 md:my-4 flex flex-col items-center">
+        <a href="/services">
         <div class="w-64 md:w-56 bg-white pt-16 pb-2 px-4 shadow-md md:shadow-2xl rounded-xl">
         <img id="icon" :src="icon" class="w-20">
         <h2 id="title" class="text-xl font-bold font-roboto text-gray-800 mt-2 line">{{ title }}</h2>
         <p id="description" class="font-source text-sm text-gray-700 font-normal mt-4">{{ description }}</p>
         <a href="" id="read-more" class="block text-4xl text-pink-600 text-right font-roboto">&#8594;</a>
         </div>
+        </a>
    </div>
 </template>
 
