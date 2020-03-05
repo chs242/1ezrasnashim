4d107d26b3e990b401b1595167f503fbf510b205
Edward Stern <edwardstern242@gmail.com>
2020-03-05 12:18:53 +0000

    responsified indexsection3

--- src/components/IndexParagraph2.vue
+++ src/components/IndexParagraph2.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="mb-4 p-4 md:w-1/2 lg:ml-40 xl:w-2/5 xl:ml-12 xl:mt-56">
+    <div class="mb-4 p-4 md:mb-0 md:pt-0 lg:py-8 xl:py-12 xl:px-12">
         <h3 id="pre-title" class="text-base text-pink-400 font-source">{{ preTitle }}</h3>
         <h1 id="title-1" class="text-3xl text-white font-source font-light leading-none mt-2">{{ title1 }}</h1>
         <p id="description" class="font-roboto text-sm text-white mt-2">{{ description }}</p>
--- src/components/IndexSection2.vue
+++ src/components/IndexSection2.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="relative lg:flex lg:flex-col lg:items-center">
         <div class="w-full relative overflow-hidden">
-            <img src="../assets/images/ambulance.jpg" class="hidden lg:block lg:w-full ambulance-img object-cover">
+            <img src="../assets/images/ambulance.jpg" class="hidden md:block md:w-full ambulance-img object-cover">
         </div>
-        <div class="links-cont w-full flex flex-col items-center px-6 py-6 md:flex-row lg:justify-center lg:absolute top-0 bottom-0">
+        <div class="links-cont w-full flex flex-col items-center px-6 py-6 md:flex-row md:justify-center md:absolute top-0 bottom-0">
             <a href="#"><img src="../assets/images/index-tab/movie-link.jpg" class="w-64 px-2 my-2 rounded-xl"></a>
             <a href="#"><img src="../assets/images/index-tab/news-link.jpg" class="w-64 my-4 px-2 rounded-xl"></a>
             <a href="#"><img src="../assets/images/index-tab/events-link.jpg" class="w-64 my-4 px-2 rounded-xl"></a>
@@ -17,6 +17,11 @@
     .ambulance-img{
         filter: opacity(20%);
         max-height: 400px;
+    }
+     @media (min-width: 1024px) and (max-width: 1800px) {
+         .ambulance-img{
+        max-height: 600px;
+    }
     }
     a{
         transition: 200ms linear;
