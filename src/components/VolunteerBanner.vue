c2c9a30b975d79c061cea4aae7b873527e8712c0
Edward Stern <edwardstern242@gmail.com>
2020-03-09 15:58:47 +0000

    created volunteersection2. responsified until lg

--- src/components/VolunteerBanner.vue
+++ src/components/VolunteerBanner.vue
@@ -2,7 +2,7 @@
     <div id="volunteer-section-banner" class="w-full mt-12 md:mt-16 lg:mt-0">
         <div class="cont w-full relative">
             <figure class="tint relative overflow-hidden">
-                <img src="../assets/images/volunteer-tab/banner-volunteer.jpg" class="banner-img h-64 md:h-auto md:w-full object-cover lg:object-none lg:w-auto lg:float-right">
+                <img src="../assets/images/volunteer-tab/banner-volunteer.jpg" class="banner-img h-64 md:h-auto md:w-full object-cover lg:object-none lg:w-auto lg:float-right lg:mr-20">
             </figure>
             <h1 class="banner-h1 text-white absolute text-center text-2xl font-source font-bold leading-none px-12 md:text-4xl md:text-right md:px-32 lg:font-light lg:px-20 lg:text-4xl"><span class="font-normal md:text-5xl lg:text-2xl lg:font-light">Become a member of our amazing</span><br class="lg:hidden"><br>Corps of Volunteers</h1>
         </div>
@@ -30,9 +30,17 @@
         right: 0;
         background: rgba(1,1,1, 0.2);
     }
+@media (min-width: 320px) and (max-width: 768px) {
+  .tint:before{
+    background: rgba(1,1,1, 0.4);
+    }
+  }
 @media (min-width: 768px) and (max-width: 1024px) {
   .banner-h1{
       top: 30%;
+    }
+    .tint:before{
+    background: rgba(1,1,1, 0.2);
     }
   }
 @media (min-width: 1024px) {
