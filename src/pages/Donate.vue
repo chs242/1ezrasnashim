fd326d59994da1a2fce00d8b6eec1f53ca52afcb
Yehuda Neufeld <newgraphil@gmail.com>
2020-03-16 12:54:41 +0200

    started working on donate form - added the top toggles

--- src/pages/Donate.vue
+++ src/pages/Donate.vue
@@ -1,8 +1,13 @@
 <template>
   <Layout :show-logo="true">
-    <DonateOptionsButtons />
-    <DonateBillingForm />
-    <DonateCC />
+    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8 py-24">
+      <DonateOptionsButtons
+        :selected-frequency.sync="selectedFrequency"
+        :selected-method.sync="selectedMethod"
+      />
+      <DonateBillingForm />
+      <!-- <DonateCC /> -->
+    </div>
   </Layout>
 </template>
 
@@ -10,6 +15,7 @@
 import DonateOptionsButtons from "~/components/Donations/DonateOptionsButtons.vue";
 import DonateBillingForm from "~/components/Donations/DonateBillingForm.vue";
 import DonateCC from "~/components/Donations/DonateCC.vue";
+import { PAYMENT_METHODS, RECURRING_FREQUENCIES } from "~/utils/constants";
 
 export default {
   name: "Donate",
@@ -17,7 +23,14 @@ export default {
     DonateOptionsButtons,
     DonateBillingForm,
     DonateCC
-  }
+  },
+  data() {
+    return {
+      selectedMethod: PAYMENT_METHODS.CC,
+      selectedFrequency: RECURRING_FREQUENCIES.MONTHLY
+    };
+  },
+  computed: {}
 };
 </script>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
 // tailwind.config.js
 module.exports = {
-    theme: {
-      fontFamily: {
-            'roboto': ['roboto', 'arial', 'san-serif'],
-            'source': ['source sans pro', 'arial', 'san-serif'],
-      },
-      extend: {
-        borderRadius: {
-          'xl': '1rem',
-        }
-      }
+  theme: {
+    fontFamily: {
+      'roboto': ['roboto', 'arial', 'san-serif'],
+      'source': ['source sans pro', 'arial', 'san-serif'],
     },
-    variants: {},
-    plugins: [],
-  }
+    extend: {
+      colors: {
+        brand: '#72298f'
+      },
+      borderRadius: {
+        'xl': '1rem',
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+}
