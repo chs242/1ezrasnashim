3fa053ed6f7ee2d688f823538c2f79b350d11782
Yehuda Neufeld <newgraphil@gmail.com>
2020-03-18 17:20:12 +0200

    added cheque payment method

--- src/components/Donations/DonateCC.vue
+++ src/components/Donations/DonateCC.vue
@@ -104,6 +104,35 @@
       <div v-show="selectedMethod == 1" class="method paypal">
         <div ref="paypalBtns" id="paypal-button-container"></div>
       </div>
+      <!-- CHEQUE -->
+      <div v-if="selectedMethod == 2" class="method cheque mb-8">
+        <p
+          class="text-sm"
+        >In order to make an offline donation we ask that you please follow these instructions:</p>
+        <ol class="list-decimal pl-4 my-4">
+          <li>
+            Make a check payable to
+            <strong>"Ezras Nashim"</strong>.
+          </li>
+          <li>
+            On the memo line of the check, please indicate that the donation is for
+            <strong>"Ezras Nashim"</strong>.
+          </li>
+          <li>
+            Please mail your check to:
+            <div class="block mt-2 p-3 bg-pink-50 rounded-lg text-pink-800">
+              <strong class="uppercase">Ezras Nashim</strong>
+              <span class="block text-sm">
+                123 G Street
+                <br />San Diego, CA 92101
+              </span>
+            </div>
+          </li>
+        </ol>
+        <p
+          class="pl-6 text-gray-700 text-sm"
+        >All contributions will be gratefully acknowledged and are tax deductible.</p>
+      </div>
     </div>
     <div v-show="state == STATES.PROCESSING" class="my-14 my-16">
       <loader />
@@ -173,9 +202,9 @@ export default {
       return this.stripeLoaded && Card;
     },
     paymentMethods() {
-      return this.plan === PLANS.ONCE
-        ? ["Credit Card", "Paypal", "Cheque"]
-        : ["Credit Card", "Paypal"];
+      return this.recurring
+        ? ["Credit Card", "Paypal"]
+        : ["Credit Card", "Paypal", "Cheque"];
     },
     complete() {
       return this.stripeComplete && this.validateEmail(this.formData.email);
@@ -304,6 +333,17 @@ export default {
     line-height: 1.5;
     &.StripeElement--focus {
       @apply shadow-outline-blue border-blue-300 outline-none;
+    }
+  }
+}
+
+.cheque {
+  ol {
+    li {
+      @apply pl-2 mt-2;
+      &::-moz-list-number {
+        @apply font-bold text-pink-600;
+      }
     }
   }
 }
