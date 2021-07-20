f8c7572948dae2c7c09005c817e0979a2e450595
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2021-07-20 08:37:31 +0300

    tweak: update donation amounts

--- src/components/Donations/DonateOptions.vue
+++ src/components/Donations/DonateOptions.vue
@@ -1,19 +1,21 @@
 <template>
-  <transition-group name="fade" class="w-full flex justify-center flex-wrap my-6">
+  <transition-group
+    name="fade"
+    class="flex flex-wrap justify-center w-full my-6"
+  >
     <div
       v-for="(desc, i) in descriptions"
       :key="amounts[i]"
-      :class="{active: amounts[i] == amount}"
+      :class="{ active: amounts[i] == amount }"
       @click.stop="$emit('update:amount', amounts[i])"
-      class="item relative w-1/3 rounded-xl bg-pink-100 p-4 m-2 cursor-pointer"
-      style="min-width: 120px;"
+      class="relative w-1/3 p-4 m-2 bg-pink-100 cursor-pointer item rounded-xl"
+      style="min-width: 120px"
     >
-      <span
-        class="text-pink-500 relative pr-1"
-        style="bottom: 0.5rem;"
-      >{{ currencySymbols[currencyIndex] }}</span>
-      <span class="text-3xl font-bold text-pink-800">{{amounts[i]}}</span>
-      <div class="text-pink-700 leading-5">{{desc}}</div>
+      <span class="relative pr-1 text-pink-500" style="bottom: 0.5rem">{{
+        currencySymbols[currencyIndex]
+      }}</span>
+      <span class="text-3xl font-bold text-pink-800">{{ amounts[i] }}</span>
+      <div class="leading-5 text-pink-700">{{ desc }}</div>
     </div>
   </transition-group>
 </template>
@@ -28,12 +30,12 @@ export default {
     return {
       currencySymbols: CURRENCY_SYMBOLS,
       allAmounts: [
-        [36, 108, 360, 450, 900, 1800], // USD
+        [54, 100, 180, 450, 900, 1800], // USD
         [50, 150, 500, 650, 1125, 2250], // CAD
         [33, 99, 330, 370, 750, 1500], // EUR
         [27, 85, 270, 310, 625, 1250], // GBP
-        [130, 390, 1300, 1500, 3000, 6000] // ILS
-      ]
+        [130, 390, 1300, 1500, 3000, 6000], // ILS
+      ],
     };
   },
   computed: {
@@ -43,21 +45,21 @@ export default {
     descriptions() {
       return !this.recurring
         ? [
-            "Sponsor a radio",
-            "Sponsor 3 radios",
-            "Sponsor 10 radios",
-            "Sponsor a month of equipment",
-            "Sponsor half an EMT course",
-            "Sponsor a whole EMT course"
+            "Sponsor a pulse-oximeter",
+            "Sponsor a tank of fuel for the ambulance",
+            "sponsor an Oxygen Tank (per year)",
+            "Sponsor a radio (per year)",
+            "Sponsor a call",
+            "Sponsor an EMT's fully-stocked Equipment bag",
           ]
         : [
-            "Sponsor a radio",
-            "Sponsor 3 radios",
-            "Sponsor 10 radios",
-            "Sponsor a month of equipment"
+            "Sponsor a pulse-oximeter",
+            "Sponsor a tank of fuel for the ambulance",
+            "sponsor an Oxygen Tank (per year)",
+            "Sponsor a radio (per year)",
           ];
-    }
-  }
+    },
+  },
 };
 </script>
 
